1190d9a122026250c06bfc897d86f717deb87cce
Andrej Ilievski <34969186+mr-andrej@users.noreply.github.com>
2020-10-12 11:44:06 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,5 +5,14 @@
 require_once 'car.php';
 require_once 'bicycle.php';
 
-$bike = new Car();
-var_dump($bike);
+$BMW = new Car("black", 5, "diesel");
+$BMX = new Bicycle();
+
+echo $BMW->start() . "<br>";
+echo $BMW->forward() . "<br>";
+echo $BMW->brake() . "<br>";
+
+echo $BMX->forward() . "<br>";
+echo $BMX->brake() . "<br>";
+
+
